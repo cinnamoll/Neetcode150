d2234f505d7bf965c3f23e36a8e8d7feb8c7a5bc
Dang Anh Que <160758822+cinnamoll@users.noreply.github.com>
2026-02-04 15:48:35 +0700

    daily

---
+++ 2_pointers/11_Container_Most_Water.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        res = 0
+        i, j = 0, len(height) - 1
+
+        while i <= j:
+            water = min(height[i], height[j]) * (j - i)
+            res = max(res, water)
+
+            if height[i] <= height[j]:
+                i+=1
+            else:
+                j-=1
+        
+        return res
