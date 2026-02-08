2de218903735257852884b812b9b1bd6a8f09b4e
Dang Anh Que <160758822+cinnamoll@users.noreply.github.com>
2026-02-08 18:28:59 +0700

    daily

---
+++ Sliding_window/424_Longest_Repeating.py
@@ -0,0 +1,13 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        res = 0
+        freq = {}
+        l = maxf = 0
+        for r in range(len(s)):
+            freq[s[r]] = 1 + freq.get(s[r], 0)
+            maxf = max(maxf, freq[s[r]])
+            while (r - l + 1) - maxf > k:
+                freq[s[l]]-=1
+                l+=1
+            res = max(res, r - l + 1)
+        return res
